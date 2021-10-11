5b32de9dd12f223b7dcbb41814738afcabfec9b6
snakehead007 <snakehead007@protonmail.com>
2021-10-11 20:04:31 +0200

    add FaunaDB

--- components/movie.js
+++ components/movie.js
@@ -3,62 +3,30 @@ import {useState} from 'react';
 import { useEffect } from 'react';
 
 
-export default function Movie({ movie_id }) {
-    //fetch movie details
-    const [movieDetails, setMovieDetails] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=84820fcd9dbc45269d08e17d64e25958&language=en`);
-                const data = await res.json();
-                setMovieDetails(data);
-                setLoading(false);
-            } catch (error) {
-                setError(true);
-            }
-        };
-        fetchData();
-    }, []);
-    
+export default function Movie({ movie }) {
+    // console.log(movie);
     return (
         <div className="mb-16">
             <div className="py-3 sm:max-w-xl sm:mx-auto">
                 <div className="bg-white shadow-lg border-gray-100 max-h-80	 border sm:rounded-3xl p-8 flex space-x-8">
                 <div className="h-48 overflow-visible w-1/2">
-                    {
-                        loading ?
-                        <div className="h-full w-full flex justify-center items-center">
-                            <div className="spinner"></div>
-                        </div>
-                        :
-                        error ?
-                        <div className="h-full w-full flex justify-center items-center">
-                            <div className="text-red-500">
-                                <p>Error fetching movie details</p>
-                            </div>
-                        </div>
-                        :
                         <Image 
                         className="rounded-3xl shadow-lg" 
                         width={600}
                         height={900}
-                        src={"https://image.tmdb.org/t/p/original"+movieDetails.poster_path} 
+                        src={"https://image.tmdb.org/t/p/original"+movie.tmdb.poster_path} 
                         alt="" />
-                    }
                 </div>
                 <div className="flex flex-col w-1/2 space-y-4">
                     <div className="flex justify-between items-start">
-                    <h2 className="text-3xl font-bold">{movieDetails.title}</h2>
-                    <div className="bg-yellow-400 font-bold rounded-xl p-2">{movieDetails.vote_average}</div>
+                    <h2 className="text-3xl font-bold">{movie.tmdb.title}</h2>
+                    <div className="bg-yellow-400 font-bold rounded-xl p-2">{movie.tmdb.vote_average}</div>
                     </div>
                     <div>
                     <div className="text-sm text-gray-400">Release date</div>
-                    <div className="text-lg text-gray-800">{movieDetails.release_date}</div>
+                    <div className="text-lg text-gray-800">{movie.tmdb.release_date}</div>
                     </div>
-                    <p className=" text-gray-400 max-h-40 overflow-y-hidden">{movieDetails.overview}</p>
+                    <p className=" text-gray-400 max-h-40 overflow-y-hidden">{movie.tmdb.overview}</p>
                 </div>
 
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import Head from 'next/head'
-import Header from '../components/header';
-import Movie from '../components/movie';
+import Head from "next/head";
+import Header from "../components/header";
+import Movie from "../components/movie";
+import fauna from "../lib/fauna";
+const {
+  Paginate,
+  Get,
+  Select,
+  Match,
+  Index,
+  Create,
+  Collection,
+  Lambda,
+  Var,
+  Join,
+  Documents,
+  Update,
+  Delete,
+  Map,
+} = fauna.query;
+const client = fauna.client;
 
-export default function Home() {
+export default function Home({ movies }) {
   return (
-    <div id="root"
+    <div
+      id="root"
       className="
       antialiased
       bg-gradient-to-r
@@ -12,17 +31,47 @@ export default function Home() {
       via-purple-300
       to-indigo-400
       h-screen
-    ">
+    "
+    >
       <Head>
         <title>Studio KaLaHa: movie reviews</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <div className="px-4">
-        <Movie movie_id={354912} />
-        <Movie movie_id={629} />
-        <Movie movie_id={424} />
+        {
+        movies.map((movie) => (
+          <Movie movie={movie} key={movie.faunadb.ref}/>
+        ))
+        }
+      </div>
     </div>
-    </div>  
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const result = await client.query(
+    Map(
+      Paginate(Documents(Collection("movie"))),
+      Lambda((x) => Get(x))
+    )
+  );
+  const movies_promised = await result.data.map(async (movieData) => {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieData.data.tmdb_id}?api_key=84820fcd9dbc45269d08e17d64e25958&language=en`);
+    const tmdb = await res.json();
+    return {
+      faunadb: {
+        data: movieData.data,
+        ref: movieData.ref.id,
+        ts: movieData.ts,
+      },
+      tmdb,
+    }
+  });
+  const movies = await Promise.all(movies_promised);
+  return {
+    props: {
+      movies,
+    },
+  };
 }
